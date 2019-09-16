b416a2c907bc9c193fc01a9541592dccdd6d8214
NovDm <novitskiydmitriy@inbox.ru>
2019-09-16 19:43:37 +0300

    Первый коммит

---
+++ Подготовка/Assets/Scripts/ONOFBUTT.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ONOFBUTT : MonoBehaviour
+{
+    public GameObject panel;
+    private bool flag=false;
+    public void OnOf()
+    {
+        if (!flag)
+        {
+            panel.SetActive(true);
+            flag = true;
+        }
+        else
+        {
+            panel.SetActive(false);
+            flag = false;
+        }
+    }
+
+   
+}
